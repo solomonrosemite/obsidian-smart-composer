32b2e33ecc631a28e01b3ea4328aefb25869521d
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2025-09-15 16:58:50 +0200

    feat: change path to hardcoded .obsidian plugin path

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import { LLMProvider, LLMProviderType } from './types/provider.types'
 export const CHAT_VIEW_TYPE = 'smtcmp-chat-view'
 export const APPLY_VIEW_TYPE = 'smtcmp-apply-view'
 
-export const PGLITE_DB_PATH = 'database/21-smart-composer/smtcmp_vector_db.tar.gz'
+export const PGLITE_DB_PATH = '.obsidian/plugins/smart-composer/smtcmp_vector_db.tar.gz'
 
 // Default model ids
 export const DEFAULT_CHAT_MODEL_ID = 'claude-sonnet-4.0'
